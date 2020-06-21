95c98b89c0c097012490f3b00ed3f023840f6725
bmaxime-c <b.maxime.c@gmail.com>
2020-06-21 17:24:40 +0200

    my first app property binding [2.27]

--- my-fiirst-app/src/app/servers/servers.component.ts
+++ my-fiirst-app/src/app/servers/servers.component.ts
@@ -2,12 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  //template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer:boolean = false;
+  
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = !this.allowNewServer;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
