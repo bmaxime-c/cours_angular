d10f18200146567a7ad4b89e2313bf4e333e10ac
bmaxime-c <b.maxime.c@gmail.com>
2020-06-21 17:43:10 +0200

    My first app Event binding [2.29]

--- my-fiirst-app/src/app/servers/servers.component.ts
+++ my-fiirst-app/src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer:boolean = false;
+  serverCreationStatus = 'No server was created!';
   
   constructor() { 
     setTimeout(() => {
@@ -18,4 +19,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 }
