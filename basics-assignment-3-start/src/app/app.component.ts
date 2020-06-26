9304bbdbe94673b4e56e6d575867626bba64b03b
bmaxime-c <b.maxime.c@gmail.com>
2020-06-26 15:45:50 +0200

    assignment 3 answer

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  passwordDisplayed: boolean = false;
+  logs = [];
+
+  tooglePassword() {
+    this.passwordDisplayed = !this.passwordDisplayed;
+    this.addLog(this.passwordDisplayed);
+  }
+
+  addLog(state) {
+    this.logs.push(this.logs.length + " - "+"Button click. password is "+(state?"shown":"hidden"));
+  }
 }
