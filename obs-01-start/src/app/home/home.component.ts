d7706730140ad1133b4f53cb8a25fe18f7eae356
bmaxime-c <b.maxime.c@gmail.com>
2020-07-09 11:09:52 +0200

    Real custom observable [13.172]

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +10,19 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   firstObsSubscription : Subscription;
   constructor() { }
-  
+
   ngOnDestroy(): void {
     this.firstObsSubscription.unsubscribe();
   }
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe((count) => {
-      console.log(count);
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {observer.next(count++)}, 1000)
+    })
+    
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
     });
     
   }
