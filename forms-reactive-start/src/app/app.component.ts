884abe3a167a2aa3dea6fb6b7a507dc145b9a8e7
bmaxime-c <b.maxime.c@gmail.com>
2020-07-10 20:16:18 +0200

    Reactive reacting to status and value change

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -26,6 +26,14 @@ export class AppComponent implements OnInit {
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+
+    // this.signUpForm.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // });
+
+    this.signUpForm.statusChanges.subscribe((value) => {
+      console.log(value);
+    });
   }
 
   onSubmit() {
