0badc0f45258cd7c4954870e0e14c85744b7810b
bmaxime-c <b.maxime.c@gmail.com>
2020-07-10 14:52:02 +0200

    using form data

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -10,6 +10,14 @@ export class AppComponent {
   @ViewChild('f', {static: true}) signupForm: NgForm;
   answer;
   genders = ['male', 'female'];
+  user = {
+    username:"",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: ""
+  }
+  submitted:boolean = false;
   
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -32,7 +40,11 @@ export class AppComponent {
   }
 
   onSubmit() {
-    console.log("Form submitted");
-    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted = true;
   }
 }
