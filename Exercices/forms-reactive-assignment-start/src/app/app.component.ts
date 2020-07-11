1f0f76ac77b8d9ccf4ea61336acd5a084953fb5e
bmaxime-c <b.maxime.c@gmail.com>
2020-07-11 12:00:00 +0200

    Practive reactive form completed [15.215]

--- Exercices/forms-reactive-assignment-start/src/app/app.component.ts
+++ Exercices/forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  theForm: FormGroup;
+  private forbiddenNames: string[] = ['Test'];
+  statuses= ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit(): void {
+    this.theForm = new FormGroup({
+      'projectname': new FormControl(null, [Validators.required, this.nameValidator.bind(this)]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectstatus': new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.theForm.value);
+  }
+
+  nameValidator(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenNames.indexOf(control.value) !== -1) {
+      return { 'nameIsForbidden': true };
+    }
+
+    return null;
+  }
 }
