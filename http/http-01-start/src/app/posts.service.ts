938fb088fa7a1b3f67f7b2576817ca2264155ef6
bmaxime-c <b.maxime.c@gmail.com>
2020-07-15 17:58:56 +0200

    Using catchError operator [18.265]

--- http/http-01-start/src/app/posts.service.ts
+++ http/http-01-start/src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -24,21 +24,25 @@ export class PostsService {
             )
             .subscribe(responseData => {
                 console.log(responseData);
-            }, (error:HttpErrorResponse) => {
+            }, (error: HttpErrorResponse) => {
                 this.error.next(error.message)
             });
     }
 
     fetchPosts() {
         return this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-28728.firebaseio.com/posts.json')
-            .pipe(map(responseData => {
-                const postsArray: Post[] = [];
-                for (const key in responseData) {
-                    postsArray.push({ ...responseData[key], id: key });
-                }
+            .pipe(
+                map(responseData => {
+                    const postsArray: Post[] = [];
+                    for (const key in responseData) {
+                        postsArray.push({ ...responseData[key], id: key });
+                    }
 
-                return postsArray;
-            }))
+                    return postsArray;
+                }), 
+                catchError(errorRes => {
+                    return throwError(errorRes);
+                }));
     }
 
     deletePosts() {
