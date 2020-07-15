7ca35715249c24db12de881acd0f5948c10c0043
bmaxime-c <b.maxime.c@gmail.com>
2020-07-15 17:30:44 +0200

    Using service for HTTP requests [18.261]

--- http/http-01-start/src/app/app.component.ts
+++ http/http-01-start/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -10,46 +11,31 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
-  isLoading: boolean = false;
+  isFetching: boolean = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private postService: PostsService) { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    this.http
-      .post<{name:string}>(
-        'https://ng-complete-guide-28728.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+    this.postService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     this.fetchPosts();
   }
 
-  onClearPosts() {
-    // Send Http request
+  private fetchPosts() {
+    this.isFetching = true;
+    this.postService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 
-  fetchPosts() {
-    this.isLoading = true;
-    this.http.get<{[key:string]: Post}>('https://ng-complete-guide-28728.firebaseio.com/posts.json')
-      .pipe(map(responseData => {
-        const postsArray: Post[] = [];
-        for (const key in responseData) {
-          postsArray.push({ ...responseData[key], id: key });
-        }
-
-        return postsArray;
-      }))
-      .subscribe(posts => {
-        this.loadedPosts = posts;
-        this.isLoading = false;
-      });
+  onClearPosts() {
+    // Send Http request
   }
 }
