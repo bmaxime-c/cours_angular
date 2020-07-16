0a00ac38660fa2f12f956c426d807c99c50fa81c
bmaxime-c <b.maxime.c@gmail.com>
2020-07-16 23:10:19 +0200

    Add token to outgoing requests [20.300]

--- prj-start/src/app/shared/data-storage.service.ts
+++ prj-start/src/app/shared/data-storage.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
-import { map, tap } from 'rxjs/operators';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {
 
     }
 
@@ -20,8 +21,14 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http.get<Recipe[]>("https://recipebook-59253.firebaseio.com/recipes.json")
-            .pipe(map(recipes => {
+        return this.authService.user.pipe(
+            take(1),
+            exhaustMap(user => {
+                return this.http.get<Recipe[]>("https://recipebook-59253.firebaseio.com/recipes.json", {
+                    params: new HttpParams().set('auth', user.token)
+                });
+            }),
+            map(recipes => {
                 return recipes.map(
                     recipe => {
                         return { ...recipe, ingredients: recipe.ingredients ?? [] };
@@ -29,6 +36,8 @@ export class DataStorageService {
             }), tap<Recipe[]>(response => {
                 this.recipeService.clearRecipes();
                 this.recipeService.addRecipes(response);
-            }));
+            })
+        );
+
     }
 }
