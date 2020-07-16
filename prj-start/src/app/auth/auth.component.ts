abae215853b94152f3a5617b5e4e62570186e111
bmaxime-c <b.maxime.c@gmail.com>
2020-07-16 22:53:56 +0200

    Reflecting auth state in the UI [20.299]

--- prj-start/src/app/auth/auth.component.ts
+++ prj-start/src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,7 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -44,6 +45,7 @@ export class AuthComponent implements OnInit {
         console.log(resData);
         form.resetForm();
         this.isLoading = false;
+        this.router.navigate(['/recipes']);
       },
       errorMessage => {
         console.error(errorMessage);
--- prj-start/src/app/header/header.component.ts
+++ prj-start/src/app/header/header.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { DataStorageService } from '../shared/data-storage.service'
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+    isAuthenticated = false;
+    userSub: Subscription;
 
-    constructor(private dataStore: DataStorageService) { }
+    constructor(private dataStore: DataStorageService, private authService: AuthService) { }
+
+    ngOnInit() {
+        this.userSub = this.authService.user
+            .subscribe(user => this.isAuthenticated =  !!user);
+        
+    }
+
+    ngOnDestroy() {
+        this.userSub.unsubscribe();
+    }
 
     onSaveData() {
         this.dataStore.storeRecipes();
