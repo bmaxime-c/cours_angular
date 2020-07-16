32adda6bbcebda0e94b02b6c2ffe3ad64c322ceb
bmaxime-c <b.maxime.c@gmail.com>
2020-07-16 12:07:29 +0200

    Response interceptors [18.273]

--- http/http-01-start/src/app/auth-interceptor.service.ts
+++ http/http-01-start/src/app/auth-interceptor.service.ts
@@ -1,6 +1,7 @@
-import { HttpRequest, HttpEvent, HttpInterceptor, HttpHandler } from '@angular/common/http';
+import { HttpRequest, HttpEvent, HttpInterceptor, HttpHandler, HttpEventType } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
@@ -12,7 +13,12 @@ export class AuthInterceptorService implements HttpInterceptor {
             .set('Auth', 'xyz')
         });
         
-        return next.handle(modifiedReq);
+        return next.handle(modifiedReq).pipe(tap(event => {
+            if(event.type === HttpEventType.Response) {
+                console.log('Response arrived, body data : ')
+                console.log(event.body);
+            }
+        }));
     }
 
 }
